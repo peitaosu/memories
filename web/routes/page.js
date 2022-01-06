a46804ab05ad6a624a2abb49f43820360f8f5919
Tony Su <peitaosu@163.com>
2022-01-06 10:08:12 +0800

    complete the login and fix indents

--- web/routes/page.js
+++ web/routes/page.js
@@ -4,7 +4,11 @@ var db = require('../database');
 var md5 = require('md5');
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Calendar' });
+    if (req.signedCookies['logged-in-id']) {
+        res.render('index', { title: 'Calendar' });
+    } else {
+        res.render('login', { title: 'Login' })
+    }
 });
 
 router.post('/login', (req, res, next) => {
@@ -27,19 +31,19 @@ router.post('/login', (req, res, next) => {
     var params = [data.email, data.password]
     db.get(sql, params, function (err, row) {
         if (err) {
-            res.status(400).json({ 'error': err.message })
+            res.status(400).json({ 'error': err.message });
             return;
         }
         if (!row) {
-            res.status(404).json({ 'error': 'user not found.' })
+            res.status(404).json({ 'error': 'user not found.' });
             return;
         }
-        res.cookie('logged-in-email', row['email'], {maxAge: 360000, signed: true}).cookie('logged-in-id', row['id'], {maxAge: 360000, signed: true}).json(row);
+        res.cookie('logged-in-email', row['email'], {maxAge: 360000, signed: true}).cookie('logged-in-id', row['id'], {maxAge: 360000, signed: true}).status(200).json(row);
     });
 });
 
 router.get('/logout', (req, res) => {
-    res.clearCookie('logged-in-email').clearCookie('logged-in-id').send('logged-in cookie cleared');
+    res.clearCookie('logged-in-email').clearCookie('logged-in-id').status(200).json({ 'message': 'success', 'data': 'logout succeed.' });
 });
 
 module.exports = router;
